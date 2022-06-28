b4fcf947594c7f7f4596b4c79d2288a6fbfa00c1
Hunter Bourgeois <bahunter@gmail.com>
2022-06-28 18:57:51 +0200

    initial commit- shoelace working

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,25 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+// import shoelace components
+import "@shoelace-style/shoelace/dist/components/button/button.js"
+import '@shoelace-style/shoelace/dist/components/card/card.js';
+import '@shoelace-style/shoelace/dist/components/icon/icon.js';
+import '@shoelace-style/shoelace/dist/components/input/input.js';
+import '@shoelace-style/shoelace/dist/components/rating/rating.js';
+import '@shoelace-style/shoelace/dist/components/alert/alert.js';
+
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
+
+// Set the base path to the folder you copied Shoelace's assets to
+setBasePath('/path/to/shoelace/dist');
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
